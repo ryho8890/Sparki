bdd1f363be2ac95ad67990456d4a1b3cffbef0f2
Athena Parker <athena.parker@colorado.edu>
2020-04-27 22:43:55 -0600

    small fixes coming back tmr

--- navigation.py
+++ navigation.py
@@ -2,7 +2,7 @@ import rospy
 import argparse
 import time
 import numpy as np
-from std_msgs.msg import Float32MultiArray, Bool
+from std_msgs.msg import *
 from geometry_msgs.msg import *
 from move_base_msgs.msg import *
 
@@ -20,42 +20,42 @@ def init():
     global publisher_navigate, subscriber_waypoints
 
     rospy.init_node('navigation')
-    publisher_navigate = rospy.Publisher('/move_base_simple/goal', PoseStamped, queue_size=10)
+    publisher_navigate = rospy.Publisher('/cmd_vel', Twist, queue_size=5) #twist
     subscriber_waypoints = rospy.Subscriber('/parkingbot/waypoints', Int32MultiArray, waypointCallBack)
 
 def waypointCallBack(data):
+    global waypoints
     # reshape and put in correct resolution
-    waypoints = np.array(data).reshape((-1, 2)) * (5 / 100.0)
+    # print(data)
+    #print(np.array(data.data))
+    waypoints = np.array(data.data).reshape((-1, 2)) * (5 / 100.0)
+    #print(waypoints)
 
 def loop():
+    global waypoints, publisher_navigate, subscriber_waypoints
 
     init()
-
     while not rospy.is_shutdown():
         start_time = time.time()
-        if waypoints is None:
-            continue
+        if waypoints is not None:
+            #continue
+            goal_x = float(waypoints[0][0])
+            goal_y = float(waypoints[0][1])
+            goal_theta = float(0)
 
+            if (goal_x and goal_y and goal_theta):
+                status = goToGoal(goal_x, goal_y, goal_theta)
         rospy.sleep(max(CYCLE_TIME - (start_time - time.time()), 0))
 
 
-    print(waypoints)
-    goal_x = float(waypoints[0][0])
-    goal_y = float(waypoints[0][1])
-    goal_theta = float(0)
 
-    print('Message sent.')
-    start_time = time.time()
-
-    if (goal_x and goal_y and goal_theta):
-        status = goToGoal(goal_x, goal_y, goal_theta)
 
     return status
 
 def goToGoal(x,y,t):
     global publisher_navigate, subscriber_waypoints
 
-    msg = PoseStamped()
+    msg = Twist()
 
     msg.header.frame_id = 'map'
     msg.pose.position.x = x
@@ -72,13 +72,13 @@ def goToGoal(x,y,t):
 
     return 1
 
-def convert2Quaternion(yaw, pitch, roll):
-    qx = np.sin(roll/2) * np.cos(pitch/2) * np.cos(yaw/2) - np.cos(roll/2) * np.sin(pitch/2) * np.sin(yaw/2)
-    qy = np.cos(roll/2) * np.sin(pitch/2) * np.cos(yaw/2) + np.sin(roll/2) * np.cos(pitch/2) * np.sin(yaw/2)
-    qz = np.cos(roll/2) * np.cos(pitch/2) * np.sin(yaw/2) - np.sin(roll/2) * np.sin(pitch/2) * np.cos(yaw/2)
-    qw = np.cos(roll/2) * np.cos(pitch/2) * np.cos(yaw/2) + np.sin(roll/2) * np.sin(pitch/2) * np.sin(yaw/2)
-
-    return [qx, qy, qz, qw]
+# def convert2Quaternion(yaw, pitch, roll):
+#     qx = np.sin(roll/2) * np.cos(pitch/2) * np.cos(yaw/2) - np.cos(roll/2) * np.sin(pitch/2) * np.sin(yaw/2)
+#     qy = np.cos(roll/2) * np.sin(pitch/2) * np.cos(yaw/2) + np.sin(roll/2) * np.cos(pitch/2) * np.sin(yaw/2)
+#     qz = np.cos(roll/2) * np.cos(pitch/2) * np.sin(yaw/2) - np.sin(roll/2) * np.sin(pitch/2) * np.cos(yaw/2)
+#     qw = np.cos(roll/2) * np.cos(pitch/2) * np.cos(yaw/2) + np.sin(roll/2) * np.sin(pitch/2) * np.sin(yaw/2)
+#
+#     return [qx, qy, qz, qw]
 
 if __name__ == "__main__":
     if not loop():
